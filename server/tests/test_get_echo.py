3d79facdb1ae0bae6c6d9c1686dd675e91054842
Live session user <ik-heet-max>
2019-08-31 16:06:12 +0000

    homework 4

--- server/tests/test_get_echo.py
+++ server/tests/test_get_echo.py
@@ -1,17 +1,23 @@
 from echo import actions
 from protocol import make_response
 import unittest
+from datetime import datetime
 
 
 class TestGetEcho(unittest.TestCase):
     
-    def test_get_echo_(self):
-        request = {
-                'data': None,
-                'time': 'some',
+    
+    request = {
+                'action': 'some_action',
                 }
-        self.assertEqual(actions.get_echo(request), make_response(request, 200, request.get('data')))
+    def test_get_echo_timestamp(self):
+        req1 = TestGetEcho.request
+        self.assertAlmostEqual(actions.get_echo(req1).get('time'), make_response(req1, 200).get('time'), 1)
 
+
+    def test_get_echo_action_and_data(self):
+        req2 = TestGetEcho.request
+        self.assertEqual(actions.get_echo(req2).get('action', 'data'), make_response(req2, 200).get('action', 'data'))
 
 if __name__ == '__main__':
     unittest.main()
